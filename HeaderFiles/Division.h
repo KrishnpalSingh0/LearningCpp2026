3eb530ffc9217700c4be0671e9bd2664088d78d2
Krishnpal Singh Bhadoriya <Krishnpal.s.b@intimetec.com>
2026-05-05 11:42:39 +0530

    HeaderFiles

---
+++ HeaderFiles/Division.h
@@ -0,0 +1,7 @@
+float divide(int num1, int num2){
+    if(num1<=0)
+    {return 0.0;}
+    else{
+        return (num1/num2);
+    }
+}
---
+++ HeaderFiles/Multiplication.h
@@ -0,0 +1,3 @@
+float mutliply(int num1, int num2){
+    return num1 * num2;
+}
