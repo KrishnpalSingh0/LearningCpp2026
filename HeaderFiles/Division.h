c2452478ba8424d6a459b8a34c017be0e299da8f
KrishnpalSingh0 <krishnpal.s.b@intimetec.com>
2026-05-08 16:11:58 +0530

    Delete HeaderFiles directory

--- HeaderFiles/Division.h
+++
@@ -1,7 +0,0 @@
-float divide(int num1, int num2){
-    if(num1<=0)
-    {return 0.0;}
-    else{
-        return (num1/num2);
-    }
-}
--- HeaderFiles/Multiplication.h
+++
@@ -1,3 +0,0 @@
-float mutliply(int num1, int num2){
-    return num1 * num2;
-}
