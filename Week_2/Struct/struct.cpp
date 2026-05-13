65234be7291066b0cfb4e99123bb45c0f312c507
Krishnpal Singh Bhadoriya <Krishnpal.s.b@intimetec.com>
2026-05-13 14:35:52 +0530

    Struct,NestedStruct

---
+++ Week_2/Struct/struct.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+struct Demo
+{
+    int num1, num2;
+    void showData()
+    {
+        cout << "the value of num1:- " << num1 << ", and num2:- " << num2 << endl;
+    }
+};
+
+struct Demo1
+{
+    struct Demo
+    {
+        int x, y;
+    } d3;
+    int num1, num2;
+};
+struct Demo2
+{
+    int val1, val2;
+    Demo1 d;
+};
+int main()
+{
+    Demo d;
+    d.num1 = 2;
+    d.num2 = 3;
+    d.showData();
+    Demo1 d1 = {{4, 8}, 10, 34};
+    cout << "Nested Struct values " << d1.d3.x << ", " << d1.d3.y << endl;
+    Demo2 d2 = {1, 4, {{1, 2}, 10, 20}};
+    cout << d2.val1 << ", " << d2.val2 << endl;
+    cout << d2.d.d3.x << ", " << d2.d.d3.y << endl;
+    cout << "Another nested struct value " << d2.d.num1 << ", " << d2.d.num2 << endl;
+    return 0;
+}
