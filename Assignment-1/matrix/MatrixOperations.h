5b5a4faf913b160df4b69882b326a3a5b62c281a
Krishnpal Singh Bhadoriya <Krishnpal.s.b@intimetec.com>
2026-05-20 16:50:35 +0530

    BankSimulation

--- Assignment-1/matrix/MatrixOperations.h
+++
@@ -1,7 +0,0 @@
-int** createMatrix(int row, int col);
-void inputMatrix(int** matrix, int row, int col);
-void displayMatrix(int** matrix, int row, int col);
-void addMatrix(int** a, int** b, int row1, int col1, int row2, int col2);
-void multiplyMatrix(int** a, int** b, int row1, int col1, int row2, int col2);
-void deleteMatrix(int** matrix, int row);
-bool isValidSize(int row, int col);
