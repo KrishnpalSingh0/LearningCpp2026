a61ad4b1d6a090e1a74930609336f73d0ef11379
KrishnpalSingh0 <krishnpal.s.b@intimetec.com>
2026-05-26 18:22:01 +0530

    Delete TestCases directory

--- TestCases/GmockTest/Test2/TestMock.h
+++
@@ -1,9 +0,0 @@
-#include "IPrinter.h"
-#include <gmock/gmock.h>
-class TestMock : public IPrinter
-{
-public:
-    MOCK_METHOD(void, printOne, (), (override));
-    MOCK_METHOD(void, printTwo, (), (override));
-    MOCK_METHOD(void, printThree, (), (override));
-};
