26be7bb664c200e6d311f8b1012d69b10f069583
krishn181 <singhkrishn704@gmail.com>
2026-06-07 14:32:26 +0530

    VirtualFileSystem

--- Coupling/LooseCoupling/IEngine.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-class IEngine
-{
-public:
-    virtual void start() = 0;
-    virtual ~IEngine() = default;
-};
--- TestCases/GmockTest/Test2/IPrinter.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-class IPrinter {
-    public:
-    virtual ~IPrinter() = default;
-    virtual void printOne() = 0;
-    virtual void printTwo() = 0;
-    virtual void printThree() = 0;;
-};
